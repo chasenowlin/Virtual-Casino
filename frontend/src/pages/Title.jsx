2d45ce04a9a201680d436685a7893f114f6c93ec
Nowlin, Chase J <chasenowlin1@gmail.com>
2026-08-03 22:24:03 -0500

    Feature: added Motion library and added button animations

--- frontend/src/pages/Title.jsx
+++ frontend/src/pages/Title.jsx
@@ -1,30 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import { motion } from "motion/react";
 
 const Title = () => {
   const navigate = useNavigate();
 
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-gray-900 to-green-900 text-white flex flex-col items-center justify-center">
-      <h1 className="font-serif text-8xl font-semibold mb-30 text-center">
+      <h1 className="font-serif tracking-wide text-8xl font-semibold mb-30 text-center">
         Virtual Casino
       </h1>
 
-      <div className="flex gap-20">
+      <div className="flex gap-50">
         {/* LOGIN chip */}
-        <div
+        <motion.div
           className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
           onClick={() => navigate("/login")}
+          whileHover={{ scale: 1.1, rotate: 360 }}
         >
           LOGIN
-        </div>
+        </motion.div>
 
         {/* SIGNUP chip */}
-        <div
+        <motion.div
           className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
           onClick={() => navigate("/signup")}
+          whileHover={{ scale: 1.1, rotate: 360 }}
         >
           SIGNUP
-        </div>
+        </motion.div>
       </div>
     </div>
   );
