d497c5329f5c78864a849de536acb86f3d4f97b8
Nowlin, Chase J <chasenowlin1@gmail.com>
2026-08-04 02:08:12 -0500

    feature: revamped background, made screen dynamic, and added movable, wiggling cards

---
+++ frontend/src/components/DancingCard.jsx
@@ -0,0 +1,33 @@
+import { motion } from "motion/react";
+
+const DancingCard = ({ size, constraints }) => {
+  return (
+    <motion.img
+      src="src/assets/card-back.png"
+      className="opacity-90"
+      style={{
+        width: `${size}em`,
+      }}
+      drag
+      dragConstraints={{
+        top: constraints[0],
+        left: constraints[1],
+        right: constraints[2],
+        bottom: constraints[3],
+      }}
+      dragElastic={0.9}
+      draggable={false}
+      animate={{
+        rotate: [0, 3, -3, 0],
+        y: [0, -5, 0],
+      }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut",
+      }}
+    />
+  );
+};
+
+export default DancingCard;
--- frontend/src/pages/Title.jsx
+++ frontend/src/pages/Title.jsx
@@ -1,35 +1,67 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
+import DancingCard from "../components/DancingCard.jsx";
 
 const Title = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full min-h-screen bg-gradient-to-b from-gray-900 to-green-900 text-white flex flex-col items-center justify-center">
-      <h1 className="font-serif tracking-wide text-8xl font-semibold mb-30 text-center">
-        Virtual Casino
-      </h1>
+    <motion.div
+      animate={{
+        background: [
+          "radial-gradient(circle at center, #2f4f4f 0%, #1e1e1e 70%)",
+          "radial-gradient(circle at center, #35654d 0%, #1e1e1e 70%)",
+          "radial-gradient(circle at center, #663d3d 0%, #1e1e1e 70%)",
+        ],
+      }}
+      transition={{
+        duration: 6,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+      }}
+      className="absolute inset-0 overflow-hidden"
+    >
+      {/* Title and Movable Cards */}
+      <div className="w-full min-h-screen text-white flex flex-col items-center justify-center">
+        <div className="flex flex-row gap-225">
+          <DancingCard size={11} constraints={[-150, -320, 1400, 650]} />
+          <DancingCard size={11} constraints={[-150, -1400, 320, 650]} />
+        </div>
+        <div className="flex flex-row gap-60">
+          <DancingCard size={11} constraints={[-325, -160, 1560, 475]} />
+          <h1 className="font-serif tracking-wide text-8xl font-semibold text-center mb-10">
+            Virtual Casino
+          </h1>
+          <DancingCard size={11} constraints={[-325, -1560, 160, 475]} />
+        </div>
 
-      <div className="flex gap-50">
-        {/* LOGIN chip */}
-        <motion.div
-          className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
-          onClick={() => navigate("/login")}
-          whileHover={{ scale: 1.1, rotate: 360 }}
-        >
-          LOGIN
-        </motion.div>
+        <div className="flex flex-row gap-225">
+          <DancingCard size={11} constraints={[-500, -320, 1400, 300]} />
+          <DancingCard size={11} constraints={[-500, -1400, 320, 300]} />
+        </div>
 
-        {/* SIGNUP chip */}
-        <motion.div
-          className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
-          onClick={() => navigate("/signup")}
-          whileHover={{ scale: 1.1, rotate: 360 }}
-        >
-          SIGNUP
-        </motion.div>
+        <div className="flex gap-50">
+          {/* LOGIN chip */}
+          <motion.div
+            className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
+            onClick={() => navigate("/login")}
+            whileHover={{ scale: 1.1, rotate: 360 }}
+          >
+            LOGIN
+          </motion.div>
+
+          {/* SIGNUP chip */}
+          <motion.div
+            className="w-48 h-48 bg-[url('src/assets/poker-chip.png')] bg-contain bg-no-repeat cursor-pointer flex items-center justify-center text-black font-bold text-xl"
+            onClick={() => navigate("/signup")}
+            whileHover={{ scale: 1.1, rotate: 360 }}
+          >
+            SIGNUP
+          </motion.div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
